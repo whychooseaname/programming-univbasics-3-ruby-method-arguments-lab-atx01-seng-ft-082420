3692328bdbf9c3df7d818d34a1c15a2222c3a9ca
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-03 21:16:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,9 +4,9 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 def introduction(name)
-  "Hi, my name is #{name}."
+  puts "Hi, my name is #{name}."
 end 
-introduction("Josh")
+
 
 
 
